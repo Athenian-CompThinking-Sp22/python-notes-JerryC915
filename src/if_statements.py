95204491549924353ee40277276b399c633ec816
Jerry <25rchen@athenian.org>
2022-04-13 12:02:39 -0700

    notes

--- src/if_statements.py
+++ src/if_statements.py
@@ -4,3 +4,16 @@
 #
 #
 #############################
+play = input("Do you want to play? ")
+if play == "No" or play == "no" or play == "NO":
+    print("I'm offended, GO AWAYYYYY. ")
+elif play == "Yes" or play == "yes" or play == "YES":   #elif = Else if
+    print("Great, lets go! ")
+else:
+    print("Say yes or no, plz!")
+
+num = int(input("Enter your favorite number: "))
+if num % 2 == 0:
+    print("even!")
+else:
+    print("Odd!")
